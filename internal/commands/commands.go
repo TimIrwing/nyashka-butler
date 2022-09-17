cd2a9e20e822498b6abc49b8004ad6f23a7213ee
Tim <timirwing@gmail.com>
2022-09-17 19:24:34 +0300

    fix: use tab char instead of spaces

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -1,35 +1,35 @@
 package commands
 
 import (
-    "log"
+	"log"
 )
 
 type Message interface {
-    New(string) Message
-    SetReply(int)
-    GetID() int
-    Send()
+	New(string) Message
+	SetReply(int)
+	GetID() int
+	Send()
 }
 
 type CommandOptions struct {
-    Args    []string
-    Message Message
+	Args    []string
+	Message Message
 }
 
 var commands = map[string]func(CommandOptions){
-    "config": func(opts CommandOptions) {
-        old := opts.Message
-        m := old.New("TODO")
-        m.SetReply(old.GetID())
-        m.Send()
-    },
+	"config": func(opts CommandOptions) {
+		old := opts.Message
+		m := old.New("TODO")
+		m.SetReply(old.GetID())
+		m.Send()
+	},
 }
 
 func Trigger(cmd string, opts CommandOptions) {
-    handler, ok := commands[cmd]
-    if !ok {
-        log.Println("No such command")
-        return
-    }
-    handler(opts)
+	handler, ok := commands[cmd]
+	if !ok {
+		log.Println("No such command")
+		return
+	}
+	handler(opts)
 }
